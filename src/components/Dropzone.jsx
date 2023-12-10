b525b0f10b124ea1f0d72b8a2e47b8d54433ff06
rafaello166 <rafaello166@interia.pl>
2023-12-10 03:03:20 +0100

    feat: avatar -> square

--- src/components/Dropzone.jsx
+++ src/components/Dropzone.jsx
@@ -1,13 +1,13 @@
-import React, {useCallback} from "react";
-import {useDropzone} from "react-dropzone";
-import {Avatar, IconButton} from "@mui/material";
-import {Delete as DeleteIcon} from "@mui/icons-material";
+import React, { useCallback } from "react";
+import { useDropzone } from "react-dropzone";
+import { IconButton } from "@mui/material";
+import { Delete as DeleteIcon } from "@mui/icons-material";
 
 const Dropzone = ({ image, setImage, file, setFile }) => {
 	const onDrop = useCallback((acceptedFiles) => {
 		setImage(URL.createObjectURL(acceptedFiles[0]));
 		setFile(acceptedFiles[0]);
-	}, []);
+	}, [setImage, setFile]);
 
 	const { getRootProps, getInputProps } = useDropzone({
 		accept: "image/jpeg, image/jpg, image/gif, image/png",
@@ -35,15 +35,14 @@ const Dropzone = ({ image, setImage, file, setFile }) => {
 			<input {...getInputProps()} />
 			{image ? (
 				<div>
-					<Avatar src={image} alt="Avatar" style={{ width: 200, height: 200 }} />
+					<img src={image} alt="Preview" style={{ width: 200, height: 200, objectFit: 'cover' }} />
 					<IconButton onClick={removeImage} style={{ position: "absolute", top: 0, right: 0 }}>
 						<DeleteIcon />
 					</IconButton>
 				</div>
 			) : (
 				<p>
-					Drag and drop an image here (or click) to update your avatar (resized to 200x200
-					automatically)
+					Drag and drop an image here (or click) to update your photo (resized to 200x200 automatically)
 				</p>
 			)}
 		</div>
